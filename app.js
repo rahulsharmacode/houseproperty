d320f7138f92b815481521aec02d8a67095a1066
rahulsharma <sharma56629@gmail.com>
2022-11-09 16:19:16 +0545

    make it better 04

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app =  express();
 require('./db/config')
 const userRouter = require('./router/userRouter')
 
-
+app.use(cors());
 app.use(express.static('./public'))
 app.use(express.json())
 app.use(userRouter);
@@ -14,7 +14,7 @@ app.use(userRouter);
 //     origin : "http://localhost:3000"
 // }))
 
-app.use(cors());
+
 
 
 app.get('/' , (req,res)=>{
